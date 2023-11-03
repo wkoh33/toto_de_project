90ad44eb596cb02853dbf7aecd724c03b28048fe
John Koh <jkoh@proofpoint.com>
2023-11-03 18:17:04 +0800

    edit image tags

--- build_and_push_docker.sh
+++ build_and_push_docker.sh
@@ -11,12 +11,18 @@ cd src
 docker-compose build
 
 # docker images
-images=("load_winning_numbers" "load_winning_outlets" "load_winning_shares")
+images=("toto_load_winning_numbers" "toto_load_winning_outlets" "toto_load_winning_shares")
 
 # tag docker image
 for image in "${images[@]}"; do
-    docker tag $image:latest $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/toto_project:$image
-    docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/toto_project:$image
+    docker tag $image:latest $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/$image:latest
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/$image:latest
+done
+
+# Clean up: Remove docker images
+for image in "${images[@]}"; do
+    docker image rm $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/$image:latest
+    docker image rm $image:latest
 done
 
 # Clean up: Logout from AWS ECR
