36504abd46b18aa1c7a43fb91f80d9a6631d3231
John Koh <jkoh@proofpoint.com>
2023-12-14 16:58:13 +0800

    Add AWS profile and logging

--- build_and_deploy_docker.sh
+++ build_and_deploy_docker.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 # Get AWS account
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+AWS_DEFAULT_PROFILE=toto
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text --profile $AWS_DEFAULT_PROFILE)
 AWS_REGION=ap-southeast-1
 
-# AWS ECR Login
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/toto_project
+# Check AWS_ACCOUNT_ID is empty
+if [ -z "$AWS_ACCOUNT_ID" ]; then
+    echo "AWS_ACCOUNT_ID is empty"
+    exit 1
+fi
+
+# AWS ECR Repo Login
+ECR_REPO=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/toto_project
+LOGIN_STATUS=$(aws ecr get-login-password --region $AWS_REGION --profile $AWS_DEFAULT_PROFILE | docker login --username AWS --password-stdin $ECR_REPO)
+
+# Check LOGIN_STATUS is "Login Succeeded"
+if [ "$LOGIN_STATUS" != "Login Succeeded" ]; then
+    echo "AWS ECR Login Failed"
+    exit 1
+fi
+
+echo "AWS ECR $LOGIN_STATUS"
 
 # Build Docker Image
+echo "Building Docker Image"
 cd src
 docker-compose build
 
@@ -20,7 +37,7 @@ for image in "${images[@]}"; do
     docker tag $image:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image:latest
     docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image:latest
 
-    aws lambda update-function-code --no-cli-pager --function-name $image --image-uri $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image:latest
+    aws lambda update-function-code --profile $AWS_DEFAULT_PROFILE --no-cli-pager --function-name $image --image-uri $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image:latest
 done
 
 # Clean up: Remove docker images
@@ -31,3 +48,5 @@ done
 
 # Clean up: Logout from AWS ECR
 docker logout $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+echo "Done"
